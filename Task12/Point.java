2dde7483bbb1014ce7e555ffd777a49326c7d2ec
ash30it <71814667+ash30it@users.noreply.github.com>
2025-11-28 03:38:03 +0000

    task 12

---
+++ Task12/Point.java
@@ -0,0 +1,22 @@
+package Task12;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public void print(){
+        System.out.println("("+x+","+y+")");
+    }
+
+    public void scale(){
+        this.x=this.x/2;
+        this.y=this.y/2;
+    }
+
+    
+}
---
+++ Task12/Task12.java
@@ -0,0 +1,11 @@
+package Task12;
+
+public class Task12 {
+    public static void main(String[] args) {
+        Point p=new Point(32,32);
+        for(int i=0;i<5;i++){
+            p.scale();
+            p.print();
+        }
+    }
+}
