51326b8059424e61eb94a2fe14d7c4f96df7d656
ash30it <71814667+ash30it@users.noreply.github.com>
2025-11-28 23:55:16 +0000

    task13

---
+++ Task13/Task13.java
@@ -0,0 +1,10 @@
+package Task13;
+
+public class Task13 {
+    public static void main(String[] args) {
+        Car myCar=new Car("Ford","Everest",2005,4);
+
+        myCar.displayDetails();    
+    }
+    
+}
