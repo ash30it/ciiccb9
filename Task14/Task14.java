bc18bd31711ca37ec03337d9e47e2b5f7baa842f
ash30it <71814667+ash30it@users.noreply.github.com>
2025-11-29 02:46:04 +0000

    mammal

---
+++ Task14/Task14.java
@@ -0,0 +1,36 @@
+package Task14;
+
+abstract class abstractClass{
+    abstract void abstractMethod();
+
+    void concreteMethod(){
+        System.out.println("This is a concrete Method.");
+    }
+}
+
+class B extends abstractClass{
+    void abstractMethod(){
+        System.out.println("B's implementation of abstractMethod.");
+    }
+}
+
+class C extends abstractClass{
+    void abstractMethod(){
+        System.out.println("C's implementation of abstractMethod.");
+    }
+}
+public class Task14 {
+    public static void main(String[] args) {
+        B b=new B();
+        b.abstractMethod();
+        b.concreteMethod();
+
+        C c=new C();
+        c.abstractMethod();
+        c.concreteMethod();
+    
+    }
+    
+
+    
+}
